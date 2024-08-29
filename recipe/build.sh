5079a8682a23f6067fdd32c37611998049436c2a
Mervin Fansler <mervin.fansler@bric.ku.dk>
2024-08-29 10:03:19 +0200

    capture matrix, tmb versions

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 export DISABLE_AUTOBREW=1
+
+## capture versions (robust to cross-compilation)
+grep "^Version:" ${PREFIX}/lib/R/library/TMB/DESCRIPTION | cut -d' ' -f2 > ${SRC_DIR}/inst/TMB-version
+grep "^Version:" ${PREFIX}/lib/R/library/Matrix/DESCRIPTION | cut -d' ' -f2 > ${SRC_DIR}/inst/Matrix-version
+
 ${R} CMD INSTALL --build . ${R_ARGS}
